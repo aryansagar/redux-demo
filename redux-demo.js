712aa1cf9347461cfafa8d22ca0061cb8e571677
aryansagar <aryansagar1996@gmail.com>
2022-03-03 13:26:24 +0530

    redux-demo

---
+++ redux-demo.js
@@ -0,0 +1,36 @@
+const redux = require('redux');
+
+
+const counterReducer = (state ={ counter : 3} , action) =>{
+if (action.type === "increment"){
+   return {
+       counter: state.counter + 1
+   };
+}
+if(action.tyoe === "decrement"){
+    return {
+        counter: state.counter - 1,
+    }
+}
+return state;
+
+};
+
+
+const store = redux.createStore(counterReducer);
+ console.log(store.getState)
+
+const counterSubscriber = () =>
+{
+   const latestState = store.getState();
+   console.log(latestState)
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({
+    type: "increament"
+});
+store.dispatch({
+    type: "decrement"
+});
